36ee33d4677d9c314528cc6e8b40230bc73b91a7
nieweglp <nieweglp@gmail.com>
2021-05-12 19:41:39 +0200

    Gum tree scrapper works and init olx scrapper. Rest api with endpoints works

--- app.py
+++ app.py
@@ -1,42 +1,50 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
-import house_scraper
-# from add import Add
+from house_scraper_gum_tree import get_urls, scrape_add
+
+
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['JSON_AS_ASCII'] = False
+
+PAGES = 2
 
 db = SQLAlchemy(app)
 
 class Add(db.Model):
     id = db.Column(db.String(), primary_key = True)
     title = db.Column(db.String())
-    # prize = db.Column(db.String())
-    # content = db.Column(db.String())
-    # added_date = db.Column(db.String())
-    # real_estate_type = db.Column(db.String())
-    # room_number = db.Column(db.String())
-    # baths_number = db.Column(db.String())
-    # parking = db.Column(db.String())
-    # size = db.Column(db.String())
-    # location = db.Column(db.String())
+    prize = db.Column(db.String())
+    content = db.Column(db.String())
+    added_date = db.Column(db.String())
+    sold_by = db.Column(db.String())
+    real_estate_type = db.Column(db.String())
+    room_number = db.Column(db.String())
+    baths_number = db.Column(db.String())
+    parking = db.Column(db.String())
+    size = db.Column(db.Integer())
+    location = db.Column(db.String())
     def to_dict(self):
-        return {'id': self.id, 'title': self.title}
+        return {'id': self.id, 'title': self.title, 'prize': self.prize,
+                'content': self.content, 'added_date': self.added_date, 
+                'sold_by': self.sold_by, 'real_estate_type': self.real_estate_type, 
+                'room_number': self.room_number, 'baths_number': self.baths_number, 
+                'parking': self.parking, 'size': self.size, 'location': self.location}
 
 
 @app.route('/scrape')
 def scrape():
-    urls = house_scraper.get_urls(pages=2)
-    adds = house_scraper.scrape_add(urls)
-    for add in adds:
+    gum_tree_urls = get_urls(pages=PAGES)
+    gum_tree_adds = scrape_add(gum_tree_urls)
+    for add in gum_tree_adds:
         adds_to_db = Add(**add)
         db.session.add(adds_to_db)
         db.session.commit()
-    return jsonify(adds)
+    return jsonify(gum_tree_adds)
 
 @app.route('/data')
 def load_data():
---
+++ house_scraper_gum_tree.py
@@ -0,0 +1,88 @@
+from bs4 import BeautifulSoup
+from requests import get
+from uuid import uuid4
+
+
+def get_urls(pages=2):
+    """Function returns add urls for given number of pages.
+    Parameters:
+        pages(int): Number of pages to scrape
+    Returns:
+        add_urls(list): List of urls
+    """
+
+    for current_page in range(1, pages):
+        url = 'https://www.gumtree.pl/s-mieszkania-i-domy-sprzedam-i-kupie/page-{0}/v1c9073p{0}'.format(current_page)
+        page = get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        base_url = 'https://www.gumtree.pl'
+        add_urls = []
+        for link in soup.find_all('a', {'class': 'href-link'}, href=True):
+            add_urls.append(base_url + link['href'])
+    return add_urls
+
+
+def translate_dict_keys(list_of_add_dicts):
+    """Function translate dictionary keys from Polish names to English.
+    Parameters:
+        list_of_add_dicts(dict): Dict of add attributtes.
+    Returns:
+        translated_add_dict(dict): Translated dict of add attributtes.
+    """
+
+    translated_keys = {'id': 'id', 'Tytuł': 'title', 'Cena': 'prize',
+                    'Opis': 'content', 'Data dodania': 'added_date',
+                    'Rodzaj nieruchomości': 'real_estate_type',
+                    'Liczba pokoi': 'room_number', 'Liczba łazienek': 'baths_number',
+                    'Parking': 'parking', 'Wielkość (m2)': 'size',
+                    'Lokalizacja': 'location', 'Na sprzedaż przez': 'sold_by'}
+    translated_add_dict = {}
+    for key in list_of_add_dicts.keys():
+        if key in translated_keys.keys():
+            translated_add_dict[translated_keys[key]] = list_of_add_dicts[key]
+    return translated_add_dict
+
+def scrape_add(add_urls):
+    """Function scrape advertisements for a given urls.
+    Parameters:
+        add_urls(list): List of add urls.
+    Returns:
+        list_of_add_dicts(list): List of add dicts.
+    """
+
+    list_of_add_dicts = []
+    for url in add_urls:
+        page_add = get(url)
+        soup_add = BeautifulSoup(page_add.text, 'html.parser', from_encoding='utf-8')
+        field_name = ['Data dodania', 'Na sprzedaż przez', 'Rodzaj nieruchomości',
+                        'Liczba pokoi', 'Liczba łazienek', 
+                        'Parking',  'Wielkość (m2)']
+
+        add_fields_dict = {}
+        add_fields_dict['id'] = str(uuid4())
+        for field in field_name:
+            try:
+                field_value = soup_add.find(text=field).findNext('span').contents[0]
+                add_fields_dict[field] = field_value
+            except:
+                continue
+
+        location = soup_add.find_all('div', {'class': 'location'})
+        for loc in location:
+            location = loc.get_text()
+
+        add_fields_dict['Lokalizacja'] = location
+
+        add_title = soup_add.find('span', class_='myAdTitle').text
+        add_fields_dict['Tytuł'] = add_title.replace('\xa0', '')
+
+        price = soup_add.find('span', class_='amount').text
+        add_fields_dict['Cena'] = price.replace('\xa0', '')
+
+        content = soup_add.find('div', class_='description').text
+        add_fields_dict['Opis'] = content
+
+        translated_add_dict_list = translate_dict_keys(add_fields_dict)
+        list_of_add_dicts.append(translated_add_dict_list)
+
+    return list_of_add_dicts
